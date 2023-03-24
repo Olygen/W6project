0b8b08b9e61be3b56e9e2ef9f90208059875b602
Oligen <olga.sarukhanova@gmail.com>
2023-03-23 20:44:49 -0400

    finally eggs are created and falling

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 const container = document.querySelector('#container');
 const basket = document.querySelector('#basket');
-const eggs = document.querySelectorAll('.egg');
 const scoreBoard = document.querySelector('#score-board');
-const leftUpButton = document.querySelector('.circle-button.left-up');
-const leftButton = document.querySelector('.circle-button.left');
-const rightUpButton = document.querySelector('.circle-button.right-up');
-const rightButton = document.querySelector('.circle-button.right');
 
 let player1Caught = 0;
 let player2Caught = 0;
@@ -13,7 +8,6 @@ let player1Missed = 0;
 let player2Missed = 0;
 let round = 1;
 let gameStatus = 'running';
-
 
 // Create Players class. Create methods to manage score and switch players:
 
@@ -28,141 +22,63 @@ class Player {
 // Create an Egg class. Create methods to manage eggs:
 
 class Egg {
-    constructor() {
-      this.shelf = null;
-    //   this.positionLeft = null;
-    //   this.positionRight = null;
-    //   this.positionBottom = null;
-    }
+  constructor() {
+    this.element = null;
+  }
 
-    // method to choose shelf randomly with values from 0 to 4 
-    chooseShelf() { 
-      this.shelf = Math.floor(Math.random() * 4) + 1;
-  
-      // Set the "id" attribute of the corresponding shelf div element
-        let shelfElementId;
-      switch (this.shelf) {
-        case 1:
-          shelfElementId = "left-up-shelf";
-          break;
-        case 2:
-          shelfElementId = "left-shelf";
-          break;
-        case 3:
-          shelfElementId = "right-up-shelf";
-          break;
-        case 4:
-          shelfElementId = "right-shelf";
-          break;
-        }
-        return shelfElementId;
-    }
-
-  // method with the loop to create instances of the class Egg
-  // with condition to recall eggs emersion after check with win and lose conditions. 
-  // initially let condition be < 20
-  // Eggs are to appear on the top of the chosen by method chooseShelf() shelves 
-
-    generateEgg() { 
-        if (i >= 20) return; //temporare!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-        // create an egg element and append the egg element to the container
-        let egg = document.createElement('div');
-        egg.className = 'egg';
-        container.appendChild(egg);
-
-        // initial eggs positions to change in generateEgg method
-        let PositionLeft = null;
-        let PositionRight = null;
-        let PositionBottom = null;
-
-        let shelfElementId = this.chooseShelf();
-
-        if (shelfElementId === "left-up-shelf") {
-            PositionLeft = 0;
-            PositionBottom = 78; 
-            PositionRight = null;
-        } else if (shelfElementId === "left-shelf") {
-            PositionLeft = 0;
-            PositionBottom = 43; 
-            PositionRight = null;
-        } else if (shelfElementId === "right-up-shelf") {
-            PositionRight = 0;
-            PositionBottom = 78; 
-            PositionLeft = null;
-        } else if (shelfElementId === "right-shelf") {
-            PositionRight = 0;
-            PositionBottom = 43; 
-            PositionLeft = null;
-        }
-
-        egg.style.left = PositionLeft + '%';
-        egg.style.right = PositionRight + '%';
-        egg.style.bottom = PositionBottom + '%';
-
-        const eggRollInterval = setInterval(() => {
-        // 1 represents the distance the egg should move on each roll
-          this.rollEgg(1); 
-        
-        // repeatedly call the this.rollEgg(1) method every 100 milliseconds, 
-        //causing the egg to roll along the shelf.
-        // checks if the egg element is still connected to the document
-          if (!egg.isConnected) { 
-            clearInterval(eggRollInterval);
-          }
-        }, 1000);
-
-        setTimeout(() => {
-            i++; //temporare!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            if (i < 20) { //temporare!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                this.generateEgg();
-              } 
-        }, 5000);
-      };
-        //method to roll eggs with the property of the edge that will be changing from 0 to 9. 
-        rollEgg(position) {
-          const eggElements = document.getElementsByClassName('egg');
-
-          for (let i = 0; i < eggElements.length; i++) {
-            const eggElement = eggElements[i];
-            const currentPosition = parseFloat(eggElement.style.left); //left!!!!!!!!!!!!!!!!!!!!!!!!
-
-            if (currentPosition >= 0 && currentPosition <= 9) {
-              const newPosition = currentPosition + position;
-
-              if (newPosition > 9) {
-                // Remove egg element temporarely once it falls off the shelf!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                let iterations = eggElement.getAttribute('data-iterations');
-                // if (!iterations) {
-                //   iterations = 0;
-                // }
-                // iterations++;
-                // eggElement.setAttribute('data-iterations', iterations);
-                // if (iterations >= 9) {
-                  eggElement.remove();
-                  continue;
-              }
-            }
-            eggElement.style.left = newPosition + '%';
-          }
-        }
+  generateEgg() {
+    const egg = document.createElement('div');
+    egg.className = 'egg';
+    container.appendChild(egg);
+    this.element = egg;
+    this.element.style.left = `${Math.floor(Math.random() * (container.offsetWidth - this.element.offsetWidth))}px`;
+  }
 };
- // eggs generation:
-let i = 0; // add loop counter variable declaration
-const egg = new Egg();
-egg.generateEgg(0);
 
-<<<<<<< HEAD
+// number of eggs set to 20:
+const maxEggs = 20;
+
+
+// eggs appear and fall immediately
+
 function generateEggWithDelay() {
-  egg.generateEgg();
-  i++;
-  if (i < maxEggs) {
-    setTimeout(generateEggWithDelay, 5000);
+  for (let i = 0; i < maxEggs; i++) {
+    setTimeout(() => {
+      const egg = new Egg();
+      egg.generateEgg();
+      eggFall(egg);
+    }, i * 1000);
   }
 }
 
-window.alert("This is a henhouse. Catch the Eggs for coming Easter \n by pressing buttons or shifting basket");
+function startEggFallAnimation() {
+  const eggs = document.querySelectorAll('.egg');
+  eggs.forEach(egg => eggFall(egg));
+}
 
+
+function eggFall(egg) {
+  egg.element.style.position = 'absolute';
+  const initialPosition = {
+    left: egg.element.offsetLeft,
+    top: egg.element.offsetTop
+  };
+  egg.element.style.left = `${initialPosition.left}px`;
+  egg.element.style.top = `${initialPosition.top}px`;
+
+  let pos = initialPosition.top;
+  let id = setInterval(frame, 5);
+  function frame() {
+    if (pos >= container.offsetHeight - 30) {
+      clearInterval(id);
+    } else {
+      pos++;
+      egg.element.style.top = `${pos}px`;
+    }
+  }
+}
+
+window.alert("You are in the henhouse. Catch the Eggs for coming Easter \n by pressing arrows");
 
 const player1Name = prompt("Enter name for player 1:");
 const player2Name = prompt("Enter name for player 2:");
@@ -172,36 +88,4 @@ const player2 = new Player(player2Name, 0, 0);
 
 const startGame = window.alert(`${player1Name} starts the game`);
 
-window.alert(`You hit the alien ship! Its hull is now ${target.hull}.`);
-
 generateEggWithDelay();
-=======
->>>>>>> parent of fe959a5 (place and steps for rollEggs method are defined)
-
-// updateScore(player, caught, missed) {
-//   // TODO: update player's score and caught/missed eggs
-// }
-
-// leftButton.addEventListener('click', () => {
-//   // TODO: move basket left
-// });
-
-// leftUpButton.addEventListener('click', () => {
-//     // TODO: move basket left-up
-//   });
-
-// rightUpButton.addEventListener('click', () => {
-//   // TODO: move basket right-up
-// });
-
-// rightButton.addEventListener('click', () => {
-//   // TODO: move basket right
-// });
-
-// eggs.forEach((egg) => {
-//   egg.addEventListener('transitionend', () => {
-//     // TODO: check if egg hit the basket or fell off the shelves
-//   });
-// });
-
-
--- scripthelper.js
+++ scripthelper.js
@@ -1,21 +1,461 @@
+const container = document.querySelector('#container');
+const basket = document.querySelector('#basket');
+const scoreBoard = document.querySelector('#score-board');
+// const leftUpButton = document.querySelector('.circle-button.left-up');
+// const leftButton = document.querySelector('.circle-button.left');
+// const rightUpButton = document.querySelector('.circle-button.right-up');
+// const rightButton = document.querySelector('.circle-button.right');
+
+let player1Caught = 0;
+let player2Caught = 0;
+let player1Missed = 0;
+let player2Missed = 0;
+let round = 1;
+let gameStatus = 'running';
+
+
+// Create Players class. Create methods to manage score and switch players:
+
+class Player {
+  constructor(name, caught, missed) {
+    this.name = name;
+    this.caught = caught;
+    this.missed = missed;
+  }
+}
+
+// Create an Egg class. Create methods to manage eggs:
+
 class Egg {
-    
-    generateEgg() {
-        // if (i >= maxEggs) return;
-
-        // create an egg element and append the egg element to the container
-        let egg = document.createElement('div');
-        egg.className = 'egg';
-        container.appendChild(egg);
-
-        i++;
+    constructor() {
+      // this.shelf = null;
     }
+
+    // method to choose shelf randomly with values from 0 to 4. 
+    // Set the "id" attribute of the corresponding shelf div element
+    // chooseShelf() { 
+    //   this.shelf = Math.floor(Math.random() * 4) + 1;
+
+    //     let shelfElementId;
+    //   switch (this.shelf) {
+    //     case 1:
+    //       shelfElementId = "left-up-shelf";
+    //       break;
+    //     case 2:
+    //       shelfElementId = "left-shelf";
+    //       break;
+    //     case 3:
+    //       shelfElementId = "right-up-shelf";
+    //       break;
+    //     case 4:
+    //       shelfElementId = "right-shelf";
+    //       break;
+    //     }
+    //     return shelfElementId;
+    // }
+
+  // method with the loop to create instances of the class Egg
+  // with condition to recall eggs emersion after check with win and lose conditions. 
+  // initially let condition be < 20
+  // Eggs are to appear on the top of the chosen by method chooseShelf() shelves 
+
+  generateEgg() { 
+
+    let egg = document.createElement('div');
+    egg.className = 'egg';
+    container.appendChild(egg);
+    egg.id = `egg-${i}`; // add a unique ID to the egg element
+    // eggFall(egg.id); 
+
+        // initial eggs positions to change in generateEgg method
+        // let PositionLeft = null;
+        // let PositionRight = null;
+        // let PositionBottom = null;
+
+        // let shelfElementId = this.chooseShelf();
+
+        // if (shelfElementId === "left-up-shelf") {
+        //     PositionLeft = 0;
+        //     PositionBottom = 78; 
+        //     PositionRight = null;
+        // } else if (shelfElementId === "left-shelf") {
+        //     PositionLeft = 0;
+        //     PositionBottom = 43; 
+        //     PositionRight = null;
+        // } else if (shelfElementId === "right-up-shelf") {
+        //     PositionRight = 0;
+        //     PositionBottom = 78; 
+        //     PositionLeft = null;
+        // } else if (shelfElementId === "right-shelf") {
+        //     PositionRight = 0;
+        //     PositionBottom = 43; 
+        //     PositionLeft = null;
+        // }
+
+        // egg.style.left = PositionLeft + '%';
+        // egg.style.right = PositionRight + '%';
+        // egg.style.bottom = PositionBottom + '%';
+
+        egg.id = `egg-${i}`; // add a unique ID to the egg element
+        // i++; 
+      
+        eggFall(egg.id); 
+
+      };
+
 };
-// eggs generation:
+// number of eggs set to 20:
 const maxEggs = 20;
 let i = 0;
 const egg = new Egg();
 
-while (i < maxEggs) {
-  egg.generateEgg(); // 
+function generateEggWithDelay() {
+  egg.generateEgg();
+  //make eggs fall
+  i++;
+  if (i < maxEggs) {
+    setTimeout(generateEggWithDelay, 500);
+  }
 }
+  
+window.alert("You are in the henhouse. Catch the Eggs for coming Easter \n by pressing arrows");
+
+
+const player1Name = prompt("Enter name for player 1:");
+const player2Name = prompt("Enter name for player 2:");
+
+const player1 = new Player(player1Name, 0, 0);
+const player2 = new Player(player2Name, 0, 0);
+
+const startGame = window.alert(`${player1Name} starts the game`);
+
+generateEggWithDelay();
+
+// get all the egg elements and their initial positions
+
+
+const eggIds = [];
+
+for (let i = 0; i < maxEggs; i++) {
+  egg.id = `egg-${i}`;
+  eggIds.push(egg.id); // add the egg ID to the array
+}
+
+function eggFall(eggId) {
+  const egg = document.getElementById(eggId); // select the egg element by ID
+  let pos = 0;
+  let id = null;
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  function frame() {
+    if (egg.offsetTop >= egg.parentNode.offsetHeight - 30) {
+      clearInterval(id);
+    } else {
+      pos++;
+      egg.style.top = egg.offsetTop + pos + "px"; 
+    }
+  }
+}
+
+// eggIds.forEach((eggId) => {
+//   eggFall(eggId);
+// });
+
+// updateScore(player, caught, missed) {
+//   // TODO: update player's score and caught/missed eggs
+// }
+
+// leftButton.addEventListener('click', () => {
+//   // TODO: move basket left
+// });
+
+// leftUpButton.addEventListener('click', () => {
+//     // TODO: move basket left-up
+//   });
+
+// rightUpButton.addEventListener('click', () => {
+//   // TODO: move basket right-up
+// });
+
+// rightButton.addEventListener('click', () => {
+//   // TODO: move basket right
+// });
+
+// eggs.forEach((egg) => {
+//   egg.addEventListener('transitionend', () => {
+//     // TODO: check if egg hit the basket or fell off the shelves
+//   });
+// });
+
+function eggFall(eggId) {
+    const egg = document.getElementById(eggId);
+    const containerHeight = container.offsetHeight;
+    const eggHeight = egg.offsetHeight;
+    let topPosition = parseInt(egg.style.top);
+    let fallSpeed = 10;
+  
+    const fall = setInterval(function() {
+      topPosition += fallSpeed;
+      egg.style.top = topPosition + "px";
+      
+      if (topPosition + eggHeight > containerHeight) {
+        clearInterval(fall);
+        egg.remove();
+      }
+    }, 50);
+  }
+// function eggFall(eggId) {
+//   const egg = document.getElementById(eggId); // select the egg element by ID
+//   let pos = 0;
+//   let id = null;
+//   clearInterval(id);
+//   id = setInterval(frame, 5);
+//   function frame() {
+//     if (egg.offsetTop >= egg.parentNode.offsetHeight - 30) {
+//       clearInterval(id);
+//     } else {
+//       pos++;
+//       egg.style.top = egg.offsetTop + pos + "px"; 
+//     }
+//   }
+// }
+  function eggFall(eggId) {
+    const egg = document.getElementById(eggId);
+    let topPos = parseInt(egg.style.top);
+    const eggFallSpeed = 5; // adjust the speed of the fall by changing this value
+    setInterval(() => {
+      topPos += eggFallSpeed;
+      egg.style.top = `${topPos}px`;
+    }, 50);
+  }
+
+  // function eggFall(eggId) {
+//   const egg = document.getElementById(eggId);
+//   let pos = 1;
+//   let id = setInterval(frame, 5);
+
+//   function frame() {
+//     if (egg.offsetTop >= egg.parentNode.offsetHeight - egg.offsetHeight) {
+//       clearInterval(id);
+//     } else {
+//       pos += 0.1;
+//       egg.style.top = egg.offsetTop + pos + "px"; 
+//     }
+//   }
+// }
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+const container = document.querySelector('#container');
+const basket = document.querySelector('#basket');
+const scoreBoard = document.querySelector('#score-board');
+
+let player1Caught = 0;
+let player2Caught = 0;
+let player1Missed = 0;
+let player2Missed = 0;
+let round = 1;
+let gameStatus = 'running';
+
+
+// Create Players class. Create methods to manage score and switch players:
+
+class Player {
+  constructor(name, caught, missed) {
+    this.name = name;
+    this.caught = caught;
+    this.missed = missed;
+  }
+}
+
+// Create an Egg class. Create methods to manage eggs:
+
+class Egg {
+    constructor() {
+      // this.shelf = null;
+    }
+
+      generateEgg() { 
+        let egg = document.createElement('div');
+        egg.className = 'egg';
+        container.appendChild(egg);
+        egg.id = `egg-${i}`;
+        egg.style.left = `${Math.floor(Math.random() * (container.offsetWidth - egg.offsetWidth))}px`;
+      };
+
+};
+
+// number of eggs set to 20:
+const maxEggs = 20;
+let i = 0;
+const egg = new Egg();
+
+// function generateEggWithDelay() {
+//   egg.generateEgg();
+//   i++;
+//   if (i < maxEggs) {
+//     setTimeout(generateEggWithDelay, 500);
+//   } else {
+//     eggIds.forEach((eggId) => {
+//       setTimeout(() => eggFall(eggId), 1000);
+//     });
+//   }
+// }
+  
+// function generateEggWithDelay() {
+//   egg.generateEgg();
+//   const eggId = `egg-${i}`;
+//   const eggElement = document.getElementById(eggId);
+//   const eggPosition = {
+//     left: parseInt(eggElement.style.left),
+//     top: parseInt(eggElement.style.top)
+//   };
+//   i++;
+//   if (i < maxEggs) {
+//     setTimeout(generateEggWithDelay, 500);
+//   } else {
+//     eggIds.forEach((eggId) => {
+//       setTimeout(() => eggFall(eggId, eggPosition), 1000);
+//     });
+//   }
+// }
+
+function generateEggWithDelay() {
+  for (let i = 0; i < maxEggs; i++) {
+    const eggId = `egg-${i}`;
+    const egg = new Egg();
+    egg.id = eggId;
+    egg.generateEgg();
+
+    const initialPosition = {
+      left: egg.offsetLeft,
+      top: egg.offsetTop
+    };
+
+    eggIds.push(eggId);
+
+    setTimeout(() => eggFall(eggId, initialPosition), 1000 + i * 1000);
+  }
+}
+
+const eggIds = [];
+
+for (let i = 0; i < maxEggs; i++) {
+  egg.id = `egg-${i}`;
+  eggIds.push(egg.id); // add the egg ID to the array
+}
+
+function eggFall(eggId, initialPosition) {
+  const egg = document.getElementById(eggId);
+  egg.style.position = 'absolute';
+  egg.style.left = `${initialPosition.left}px`; 
+  egg.style.top = `${initialPosition.top}px`; 
+
+  let pos = initialPosition.top;
+  let id = setInterval(frame, 5);
+  function frame() {
+    if (pos >= container.offsetHeight - 30) {
+      clearInterval(id);
+    } else {
+      pos++;
+      egg.style.top = `${pos}px`;
+    }
+  }
+}
+
+
+window.alert("You are in the henhouse. Catch the Eggs for coming Easter \n by pressing arrows");
+
+
+const player1Name = prompt("Enter name for player 1:");
+const player2Name = prompt("Enter name for player 2:");
+
+const player1 = new Player(player1Name, 0, 0);
+const player2 = new Player(player2Name, 0, 0);
+
+const startGame = window.alert(`${player1Name} starts the game`);
+
+generateEggWithDelay();
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+const container = document.querySelector('#container');
+const basket = document.querySelector('#basket');
+const scoreBoard = document.querySelector('#score-board');
+
+let player1Caught = 0;
+let player2Caught = 0;
+let player1Missed = 0;
+let player2Missed = 0;
+let round = 1;
+let gameStatus = 'running';
+
+// Create Players class. Create methods to manage score and switch players:
+
+class Player {
+  constructor(name, caught, missed) {
+    this.name = name;
+    this.caught = caught;
+    this.missed = missed;
+  }
+}
+
+// Create an Egg class. Create methods to manage eggs:
+
+class Egg {
+  constructor() {
+    this.element = null;
+  }
+
+  generateEgg() {
+    const egg = document.createElement('div');
+    egg.className = 'egg';
+    container.appendChild(egg);
+    this.element = egg;
+    this.element.style.left = `${Math.floor(Math.random() * (container.offsetWidth - this.element.offsetWidth))}px`;
+  }
+};
+
+// number of eggs set to 20:
+const maxEggs = 20;
+
+function generateEggWithDelay() {
+  for (let i = 0; i < maxEggs; i++) {
+    const egg = new Egg();
+    egg.generateEgg();
+
+    setTimeout(() => eggFall(egg), 1000 + i * 1000);
+  }
+}
+
+function eggFall(egg) {
+  egg.element.style.position = 'absolute';
+  const initialPosition = {
+    left: egg.element.offsetLeft,
+    top: egg.element.offsetTop
+  };
+  egg.element.style.left = `${initialPosition.left}px`;
+  egg.element.style.top = `${initialPosition.top}px`;
+
+  let pos = initialPosition.top;
+  let id = setInterval(frame, 5);
+  function frame() {
+    if (pos >= container.offsetHeight - 30) {
+      clearInterval(id);
+    } else {
+      pos++;
+      egg.element.style.top = `${pos}px`;
+    }
+  }
+}
+
+window.alert("You are in the henhouse. Catch the Eggs for coming Easter \n by pressing arrows");
+
+const player1Name = prompt("Enter name for player 1:");
+const player2Name = prompt("Enter name for player 2:");
+
+const player1 = new Player(player1Name, 0, 0);
+const player2 = new Player(player2Name, 0, 0);
+
+const startGame = window.alert(`${player1Name} starts the game`);
+
+generateEggWithDelay();
